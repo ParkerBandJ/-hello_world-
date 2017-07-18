32187cf47e7989f7b452b7aaaf1b4049b42f8b43
ParkerBandJ <parkerbarrett04@gmail.com>
2017-07-18 10:57:40 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+* File: index.js
+* Author: Parker_Barrett
+* Purpose: This is my hello, world application
+* History: 7/17/2017 - File Created
+*
+*/
+
+var r = ' '
+var e = 'Enough of my rambling.';
+var w = 'job is to add or subtract two values. Oh! How rude of me! I should probably let you start your calculating.';
+var q = '     Hello my friend(s). My name is Callie. As you can provably guess, that is short for Calculator. My ';
+var c = 'What is 38 + 63?';
+var a = 38;
+var b = 63;
+var d = '  -CALCULATOR- ';
+var ttl = a + b;
+
+console.log (q)
+
+console.log (w)
+
+console.log (e)
+
+console.log (r)
+
+console.log (d);
+
+console.log (c);
+
+console.log (a + ' + ' + b + ' = ' + ttl);
+
+if (ttl == a + b) {
+    console.log ('True');
+}
+else {console.log ('False')};
